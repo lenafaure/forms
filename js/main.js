4b13c5b693859f24c8740ab2b8390907e1da59a1
Lena Faure <lena.faure@gmail.com>
2016-12-31 16:10:21 +0100

    Finish Login form and work on other forms

--- js/main.js
+++ js/main.js
@@ -2,62 +2,57 @@
  * Created by lenafaure on 30/12/2016.
  */
 
-/* sign-up form validation */
+/* form validation */
 
-var signupForm = document.getElementById('signup');
-var inputFirstName = document.getElementById('input-first-name');
-var inputLastName = document.getElementById('input-last-name');
+var form = document.getElementsByTagName('form')[0];
+var inputSimpleCheck = document.querySelectorAll('.input-simple-check');
 var inputEmail = document.getElementById('input-email');
 var inputBirthDate = document.getElementById('input-birthdate');
+var inputScheduleDate = document.getElementById('input-schedule-date');
+var inputScheduleTime = document.getElementById('input-schedule-time');
+var inputPassword = document.getElementById('input-password');
+var inputPhoneNumber = document.getElementById('input-phone-number');
 
-signupForm.addEventListener("keydown", function (e) {
+console.log(inputScheduleTime);
+
+inputScheduleTime.addEventListener("change", function(){
+    console.log(inputScheduleTime.value);
+});
+
+form.addEventListener("keydown", function (e) {
     if (e.keyCode == 13) {
         e.preventDefault();
         return false;
     }
 });
 
-signupForm.addEventListener('submit', function (event) {
+form.addEventListener('submit', function (event) {
     // stop the event from its default action: submitting the form (for our validation, submission is not desired)
     event.preventDefault();
 });
 
 
-/* first name */
+/* simple check (check if not empty) */
 
-function checkFirstName(input){
+function checkSimple(input){
     var element = input.value;
 
     if (validator.isEmpty(element)) {
         input.setCustomValidity("This field is required");
-        input.className = "input-form";
+        input.className = "input-form input-simple-check";
     } else {
         input.setCustomValidity("");
-        input.className = "input-form valid";
+        input.className = "input-form input-simple-check valid";
     }
 }
 
-inputFirstName.addEventListener("keyup", function() {
-    checkFirstName(this);
-});
-
-/* first name */
-
-function checkLastName(input){
-    var element = input.value;
-
-    if (validator.isEmpty(element)) {
-        input.setCustomValidity("This field is required");
-        input.className = "input-form";
-    } else {
-        input.setCustomValidity("");
-        input.className = "input-form valid";
-    }
+if(inputSimpleCheck){
+    for(var i = 0; i < inputSimpleCheck.length; i++)
+    inputSimpleCheck[i].addEventListener("keyup", function() {
+        checkSimple(this);
+    });
 }
 
-inputLastName.addEventListener("keyup", function() {
-    checkLastName(this);
-});
 
 /* email */
 
@@ -79,11 +74,14 @@ function checkEmail(input){
 
 }
 
-inputEmail.addEventListener("keyup", function() {
-    checkEmail(this);
-});
+if(inputEmail){
+    inputEmail.addEventListener("keyup", function() {
+        checkEmail(this);
+    });
+}
 
-/* date of birth */
+
+/* birth date */
 
 function checkBirthDate(input){
     var element = input.value;
@@ -93,12 +91,12 @@ function checkBirthDate(input){
         input.className = "input-form";
     } else {
         if (validator.isDate(element)) {
-            if(validator.isBeforeToday(element)){
+            if(validator.isBeforeToday(element) && validator.isAfterDate(element, "01/01/1900")){
                 input.setCustomValidity("");
                 input.className = "input-form valid";
             }
             else{
-                input.setCustomValidity("You are not born yet...");
+                input.setCustomValidity("Please enter a date between 1900 and now");
                 input.className = "input-form";
             }
 
@@ -110,6 +108,94 @@ function checkBirthDate(input){
 
 }
 
-inputBirthDate.addEventListener("focusout", function() {
-    checkBirthDate(this);
-});
+if(inputBirthDate){
+    inputBirthDate.addEventListener("change", function() {
+        checkBirthDate(this);
+    });
+}
+
+/* schedule date */
+
+function checkScheduleDate(input){
+    var element = input.value;
+
+    if (validator.isEmpty(element)) {
+        input.setCustomValidity("This field is required");
+        input.className = "input-form";
+    } else {
+        if (validator.isDate(element)) {
+            if(validator.isAfterToday(element)){
+                input.setCustomValidity("");
+                input.className = "input-form valid";
+            }
+            else{
+                input.setCustomValidity("This date has already passed");
+                input.className = "input-form";
+            }
+
+        } else {
+            input.setCustomValidity("Please enter a valid date");
+            input.className = "input-form";
+        }
+    }
+}
+
+if(inputScheduleDate){
+    inputScheduleDate.addEventListener("change", function() {
+        checkScheduleDate(this);
+    });
+}
+
+
+
+/* password */
+
+function checkPassword(input){
+    var element = input.value;
+
+    if (validator.isEmpty(element)) {
+        input.setCustomValidity("This field is required");
+        input.className = "input-form";
+    }
+    else {
+        if(input.validity.valid){
+            input.setCustomValidity("");
+            input.className = "input-form valid";
+        }
+        else {
+            input.className = "input-form";
+        }
+    }
+}
+
+if(inputPassword){
+    inputPassword.addEventListener("keyup", function() {
+        checkPassword(this);
+    });
+}
+
+
+/* phone number */
+
+function checkPhoneNumber(input){
+    var element = input.value;
+
+    if (validator.isEmpty(element)) {
+        input.setCustomValidity("This field is required");
+        input.className = "input-form";
+    } else {
+        if (validator.isPhoneNumber(element)) {
+            input.setCustomValidity("");
+            input.className = "input-form valid";
+        } else {
+            input.setCustomValidity("Please enter a valid phone number");
+            input.className = "input-form";
+        }
+    }
+}
+
+if(inputPhoneNumber){
+    inputPhoneNumber.addEventListener("keyup", function() {
+        checkPhoneNumber(this);
+    });
+}
